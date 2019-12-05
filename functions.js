a83cd424b09e3545b0f42171a5fe7a0060ff8180
Olga-200 <oli4ka3110@gmail.com>
2019-12-05 19:10:00 +0200

    comments

---
+++ functions.js
@@ -0,0 +1,8 @@
+function addTask(newTask, id) {
+	let taskElement = document.createElement('li');
+	taskElement.classList.add('list-group-item');
+	taskElement.innerText = newTask.title;
+
+	let taskContainer = document.querySelector(`[data-status="${newTask.status}"]`);
+	taskContainer.appendChild(taskElement);
+}
